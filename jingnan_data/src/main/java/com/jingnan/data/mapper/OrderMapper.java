69aed885769a5554f6fe135b4c1ad01d678d4052
chz <chenghongzhan@zprayder.com>
2021-12-03 10:06:51 +0800

    Initial commit

---
+++ jingnan_data/src/main/java/com/jingnan/data/mapper/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.jingnan.data.mapper;
+
+import com.jn.pojo.Order;
+import com.jn.pojo.User;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+/**
+ * @Author: sublun
+ * @Date: 2021/5/21 15:55
+ */
+public interface OrderMapper extends Mapper<Order> {
+
+
+}
